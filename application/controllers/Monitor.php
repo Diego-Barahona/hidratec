0d55606ba975bc2940439e0ec592c13be3da2151
DESKTOP-JE87G1M\Sebastian Cabello <scabello58@gmail.com>
2021-08-09 12:46:45 -0400

    project KPI

---
+++ application/controllers/Monitor.php
@@ -0,0 +1,67 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+class Monitor extends CI_Controller {
+
+public function __construct(){
+    parent:: __construct(); 
+}
+
+   public function viewProjector()
+
+    { 
+    
+        if ($this->accesscontrol->checkAuth()['correct']) {
+ 
+            $this->load->view('admin/projector');
+ 
+        } else {
+           redirect('Home/login', 'refresh');
+        }
+    }
+
+
+
+
+    public function chartQuotation(){
+
+        if ($this->accesscontrol->checkAuth()['correct']) {
+
+            $this->load->model('ProjectorModel');
+            $datos = $this->ProjectorModel->getKpiQuotation();
+            $this->response->sendJSONResponse($datos);
+
+        } else {
+
+            $this->response->sendJSONResponse(array('msg' => 'Permisos insuficientes'), 400);
+        }
+    }
+
+
+    public function chartProduction(){
+        
+        if ($this->accesscontrol->checkAuth()['correct']) {
+            $this->load->model('ProjectorModel');
+            $datos = $this->ProjectorModel->getKpiProduction();
+            $this->response->sendJSONResponse($datos);
+        } else {
+            $this->response->sendJSONResponse(array('msg' => 'Permisos insuficientes'), 400);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ application/models/ProjectorModel.php
@@ -0,0 +1,62 @@
+<?php
+class ProjectorModel extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+    public function getKpiQuotation() { 
+
+
+        // date_admission -> ingreso data 
+        // date_quotation -> fecha de cotizacion 
+        // date _reparation -> fecha reparacion 
+        // date_cellar -> fecha de bodega 
+        // days_quotation y days_reparation 
+       
+        
+        $query1 = "SELECT  AVG(5 * (DATEDIFF( ot.date_quotation,ot.date_admission) DIV 7) + MID('0123455401234434012332340122123401101234000123450', 7 * WEEKDAY(ot.date_admission) + WEEKDAY(ot.date_quotation) + 1, 1)) as kpi_quotation
+                   FROM ot
+                   WHERE  ot.date_quotation IS NOT NULL 
+                   ";
+
+        $value = $this->db->query($query1)->row_array();
+         
+
+        return $value ;
+        
+    
+         // suma todas las condiciones  sera el valor final 
+
+    }
+
+
+
+
+    public function getKpiReparation() { 
+
+        // date_admission -> ingreso data 
+        // date_quotation -> fecha de cotizacion 
+        // date _reparation -> fecha reparacion 
+        // date_cellar -> fecha de bodega 
+        $query1 = "SELECT  AVG(5 * (DATEDIFF( ot.date_cellar,ot.date_reparation) DIV 7) + MID('0123455401234434012332340122123401101234000123450', 7 * WEEKDAY(ot.date_reparation) + WEEKDAY(ot.date_cellar) + 1, 1)) as kpi_reparation
+                   FROM ot
+                   WHERE  ot.date_quotation IS NOT NULL 
+                   ";
+
+        $value = $this->db->query($query1)->row_array();
+        return $value ;
+         // suma todas las condiciones  sera el valor final 
+
+    }
+
+
+
+
+
+
+ 
+}
